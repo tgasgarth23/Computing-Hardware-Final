90d9b50363952c95f6cb34389c7839434c4606a6
JaxtonHale <theslammer100@gmail.com>
2023-11-14 17:48:11 -0500

    improved script, added angry lebron pic

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-
-
 from feat import Detector
-
-detector = Detector(
-    face_model="retinaface",
-    landmark_model="mobilefacenet",
-    au_model='xgb',
-    emotion_model="resmasknet",
-    facepose_model="img2pose",
-)
-
-from feat.utils.io import get_test_data_path
 from feat.plotting import imshow
 import os
 
-# Helper to point to the test data folder
-test_data_dir = get_test_data_path()
 
-# Get the full path
-single_face_img_path = os.path.join(os.getcwd(), "lebron.png")
+def get_prediction(image_name):
+    detector = Detector(
+        face_model="retinaface",
+        landmark_model="mobilefacenet",
+        au_model='xgb',
+        emotion_model="resmasknet",
+        facepose_model="img2pose",
+    )
 
-# Plot it
-imshow(single_face_img_path)
-
-single_face_prediction = detector.detect_image(single_face_img_path)
-
-print(single_face_prediction.emotions)
-
+    single_face_img_path = os.path.join(os.getcwd(), image_name)
+    return detector.detect_image(single_face_img_path)
 
 def is_satisfied():
-    return False
+    user_emotions = get_prediction("lebronmad.jpg").emotions
+    print(user_emotions)
+
+    positive_factor = user_emotions["happiness"] + user_emotions["surprise"]
+    negative_factor = user_emotions["anger"] + user_emotions["disgust"] + user_emotions["sadness"]
+
+    return (positive_factor > negative_factor).bool()
+
+if is_satisfied():
+    print("User is satisfied by the Val menu options for today!")
+else:
+    print("User is discontent with the Val menu options for today!")
