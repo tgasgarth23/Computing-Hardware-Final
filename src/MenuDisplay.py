6ea2096a243c902929a381f2ab1202f62a5feff0
Tgasgarth23 <tgasgarth23@amherst.edu>
2023-12-12 20:41:31 -0500

    fixes

--- src/MenuDisplay.py
+++ src/MenuDisplay.py
@@ -124,13 +124,11 @@ class MenuDisplay(tk.Tk):
         #cv2.imshow("Frame", frame)  # Display the frame for debug purposes
         #cv2.waitKey(1)
         emotions = self.emotion_analyzer.analyze_image(frame)
-        print("Emotions are: ")
-        print(emotions)
-
-        print(self.selected_option)
-        if emotions != []:
+        
+        if len(emotions) > 0:
+            print(f"You're feeling {emotions} about {self.selected_option}.")
             self.data_manager.save_to_csv(emotions, self.selected_option)
-
+            
             self.return_to_main_screen()
         else:
             messagebox.showinfo("Information", "Face not recognized. Please try again!")
